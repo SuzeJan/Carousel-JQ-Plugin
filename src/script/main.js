0c8b8cef59148d8a39114d90b561f30c6f4d3214
xiaoxu <932034728@qq.com>
2017-02-09 21:50:37 +0800

    轮播图

---
+++ src/script/main.js
@@ -0,0 +1,15 @@
+/**
+ * Created by 93203 on 2016/10/31.
+ */
+window.onload = function () {
+  $('#carousel').Carousel({
+    width: 1200,
+    height: 400,
+    imgWidth: 800,
+    imgHeight: 400,
+    scale: .6,
+    speed: 300,
+    autoPlayDelay: 4000,
+    autoPlay: true
+  });
+};
